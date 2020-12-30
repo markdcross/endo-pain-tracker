ea949606e87323d8698e8f88e953bbd1fea1fce8
MDC <markdcross@gmail.com>
2020-12-30 13:41:26 -0500

    Routes defined, models and routes dirs created

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send({ msg: `Welcome to Amy's pain tracker` }));
 
+// Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/entries', require('./routes/entries'));
+
 const PORT = process.env.PORT || 6000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
