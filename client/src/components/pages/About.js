b0f7d1bdb285884a1117ba722455bf2f4c6da935
MDC <markdcross@gmail.com>
2020-12-31 14:01:38 -0500

    Created React app

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const About = () => {
+  return (
+    <div>
+      <h1>About This App</h1>
+      <p className='my-1'>
+        This is a full stack React app for keeping track of pain caused by endometriosis and the associated aggravating factors . 
+      </p>
+      <p className='bg-dark p'>
+        <strong>Version</strong> 1.0.0
+      </p>
+    </div>
+  );
+};
+
+export default About;
